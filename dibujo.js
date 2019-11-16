1253051f9865093c92e8c033fc350a7f9144356a
Yuli <yulimart@Jorges-MacBook-Pro.local>
2019-11-15 19:15:41 -0500

    segundo commit

--- dibujo.js
+++ dibujo.js
@@ -1,27 +1,10 @@
-var d = document.getElementById("dibujito");
-var lienzo = d.getContext("2d");
-var lineas = 60;
-var l = 0;
-var xinicial, yfinal;
-var colorlinea = "red";
+var texto = document.getElementById("texto_formulario"); // getElementById se utiliza para enlazar el archivo js con html
+var boton = document.getElementById("boton_formulario");
+boton.addEventListener("click", dibujoPorClick); // Aqui determinamos bajo que evento el "Boton" traera la funcion
+var dibujo = document.getElementById("dibujito");
+var ancho_canvas = dibujo.width;
+var lienzo = dibujo.getContext("2d"); // Definimos el contexto del dibujo (2d) que sera trazado con la variable lienzo
 
-for(l = 0; l < lineas; l++)
-{
-  xinicial = 10 * l;
-  yinicial = 10 * l;
-  xfinal = 10 * (l + 1)
-  yfinal = 500 - (l * 10)
-  dibujarLinea("red", 500, yinicial, xfinal, 0);
-  dibujarLinea("orange", 0, yinicial, xfinal, 500);
-  dibujarLinea("blue", xinicial, 500, 500, yfinal);
-  dibujarLinea("green", 0, yfinal, xfinal, 0);
-}
-
-dibujarLinea("black", 499, 499, 499, 1);
-dibujarLinea("black", 499, 1, 1, 1);
-dibujarLinea("black", 1, 1, 1, 499);
-dibujarLinea("black", 1, 499, 499, 499);
-consola.log("Linea " + l);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -31,4 +14,33 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.lineTo(xfinal, yfinal);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+  var lineas = parseInt(texto.value); //parseInt convierte el texto en numero
+  var l = 0;
+  var xinicial, yfinal;
+  var colorlinea = "red";
+  var espacio = ancho_canvas / lineas;
+  var borde = ancho_canvas - 1;
+
+  for(l = 0; l < lineas; l++) // for se utiliza para hacer ciclos, hay otros como while o do while
+  {
+    xinicial = espacio * l;
+    yinicial = espacio * l;
+    xfinal = espacio * (l + 1)
+    yfinal = 500 - (l * espacio)
+    dibujarLinea("red", 500, yinicial, xfinal, 0);
+    dibujarLinea("orange", 0, yinicial, xfinal, 500);
+    dibujarLinea("blue", xinicial, 500, 500, yfinal);
+    dibujarLinea("green", 0, yfinal, xfinal, 0);
+  }
+
+  dibujarLinea("black", ancho_canvas, ancho_canvas, ancho_canvas, 1);
+  dibujarLinea("black", ancho_canvas, 1, 1, 1);
+  dibujarLinea("black", 1, 1, 1, ancho_canvas);
+  dibujarLinea("black", 1, ancho_canvas, ancho_canvas, ancho_canvas);
+  console.log("Linea " + l);
+
 }
